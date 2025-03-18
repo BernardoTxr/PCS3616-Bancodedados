6d9c51d3ac2ad89433475c2a438697fd0e981d05
BernardoTxr <bernardoteixeira@usp.br>
2025-03-18 13:36:40 -0300

    terminado quase tudo do backend

--- backend/app.py
+++ backend/app.py
@@ -14,24 +14,53 @@ db.init_app(app)
 
 
 # Definindo as classes do modelo
-class Vendedor(db.Model):
-    id_vendedor = db.Column(db.Integer, primary_key=True)
-    nome_vendedor = db.Column(db.String(100), nullable=False)
-    cpf_vendedor = db.Column(db.String(11), unique=True, nullable=False)
-    cidade_vendedor = db.Column(db.String(100), nullable=False)
+class Comissao(db.Model):
+    id_comissao = db.Column(db.Integer, primary_key=True)
+    nome_comissao = db.Column(db.String(100), nullable=False)
+    cpf_comissao = db.Column(db.String(11), unique=True, nullable=False)
+    cargo_comissao = db.Column(db.String(100), nullable=False)
+    id_modalidade = db.Column(
+        db.Integer, db.ForeignKey("modalidade.id_modalidade"), nullable=False
+    )
 
 
-class Classe(db.Model):
-    id_classe = db.Column(db.Integer, primary_key=True)
-    descricao_classe = db.Column(db.String(100), unique=True, nullable=False)
+class Atleta(db.Model):
+    id_atleta = db.Column(db.Integer, primary_key=True)
+    nome_atleta = db.Column(db.String(100), nullable=False)
+    cpf_atleta = db.Column(db.String(11), unique=True, nullable=False)
+    curso_atleta = db.Column(db.String(100), nullable=False)
+    nusp_atleta = db.Column(db.Integer, unique=True, nullable=False)
 
 
-class Produto(db.Model):
-    id_produto = db.Column(db.Integer, primary_key=True)
-    descricao_produto = db.Column(db.String(200), nullable=False)
-    id_classe = db.Column(db.Integer, db.ForeignKey("classe.id_classe"), nullable=False)
-    id_vendedor = db.Column(
-        db.Integer, db.ForeignKey("vendedor.id_vendedor"), nullable=False
+class Modalidade(db.Model):
+    id_modalidade = db.Column(db.Integer, primary_key=True)
+    nome_modalidade = db.Column(db.String(100), nullable=False)
+    saldo_modalidade = db.Column(db.Float, nullable=False)
+
+
+class Modalidade_Atleta(db.Model):
+    id_modalidade_atleta = db.Column(db.Integer, primary_key=True)
+    id_modalidade = db.Column(
+        db.Integer, db.ForeignKey("modalidade.id_modalidade"), nullable=False
+    )
+    id_atleta = db.Column(db.Integer, db.ForeignKey("atleta.id_atleta"), nullable=False)
+
+
+class Campeonato(db.Model):
+    id_campeonato = db.Column(db.Integer, primary_key=True)
+    nome_campeonato = db.Column(db.String(100), nullable=False)
+    data_inicio = db.Column(db.DateTime, nullable=False)
+    data_fim = db.Column(db.DateTime, nullable=False)
+    custo_por_pessoa = db.Column(db.Float, nullable=False)
+
+
+class Modalidade_Campeonato(db.Model):
+    id_modalidade_campeonato = db.Column(db.Integer, primary_key=True)
+    id_campeonato = db.Column(
+        db.Integer, db.ForeignKey("campeonato.id_campeonato"), nullable=False
+    )
+    id_modalidade = db.Column(
+        db.Integer, db.ForeignKey("modalidade.id_modalidade"), nullable=False
     )
 
 
@@ -43,122 +72,332 @@ with app.app_context():
 # ---------------- ROTAS ----------------
 
 
-# ✅ Listar todos os vendedores
-@app.route("/vendedores", methods=["GET"])
-def get_vendedores():
-    vendedores = Vendedor.query.all()
-    return jsonify([{"id": v.id_vendedor, "nome": v.nome_vendedor} for v in vendedores])
-
-
-# ✅ Adicionar um novo vendedor
-@app.route("/vendedores", methods=["POST"])
-def add_vendedor():
+# crud para atleta
+@app.route("/atleta", methods=["POST"])
+def create_atleta():
     data = request.json
-    vendedor = Vendedor(
-        nome_vendedor=data["nome_vendedor"],
-        cpf_vendedor=data["cpf_vendedor"],
-        cidade_vendedor=data["cidade_vendedor"],
+    atleta = Atleta(
+        nome_atleta=data["nome_atleta"],
+        cpf_atleta=data["cpf_atleta"],
+        curso_atleta=data["curso_atleta"],
+        nusp_atleta=data["nusp_atleta"],
     )
-    db.session.add(vendedor)
+    db.session.add(atleta)
     db.session.commit()
-    return jsonify({"message": "Vendedor adicionado com sucesso!"})
+    return jsonify({"message": "Atleta criado com sucesso!"})
 
 
-# ✅ Listar todas as classes de produtos
-@app.route("/classes", methods=["GET"])
-def get_classes():
-    classes = Classe.query.all()
+@app.route("/atleta", methods=["GET"])
+def read_atleta():
+    atletas = Atleta.query.all()
+    atletas = [
+        {"id_atleta": atleta.id_atleta, "nome_atleta": atleta.nome_atleta}
+        for atleta in atletas
+    ]
+    return jsonify(atletas)
+
+
+@app.route("/atleta/<int:id_atleta>", methods=["GET"])
+def read_atleta_id(id_atleta):
+    atleta = Atleta.query.get(id_atleta)
+    return jsonify({"id_atleta": atleta.id_atleta, "nome_atleta": atleta.nome_atleta})
+
+
+@app.route("/atleta/<int:id_atleta>", methods=["PUT"])
+def update_atleta(id_atleta):
+    data = request.json
+    atleta = Atleta.query.get(id_atleta)
+    atleta.nome_atleta = data["nome_atleta"]
+    atleta.cpf_atleta = data["cpf_atleta"]
+    atleta.curso_atleta = data["curso_atleta"]
+    atleta.nusp_atleta = data["nusp_atleta"]
+    db.session.commit()
+    return jsonify({"message": "Atleta atualizado com sucesso!"})
+
+
+@app.route("/atleta/<int:id_atleta>", methods=["DELETE"])
+def delete_atleta(id_atleta):
+    atleta = Atleta.query.get(id_atleta)
+    db.session.delete(atleta)
+    db.session.commit()
+    return jsonify({"message": "Atleta deletado com sucesso!"})
+
+
+# crud para comissao
+@app.route("/comissao", methods=["POST"])
+def create_comissao():
+    data = request.json
+    comissao = Comissao(
+        nome_comissao=data["nome_comissao"],
+        cpf_comissao=data["cpf_comissao"],
+        cargo_comissao=data["cargo_comissao"],
+        id_modalidade=data["id_modalidade"],
+    )
+    db.session.add(comissao)
+    db.session.commit()
+    return jsonify({"message": "Comissão criada com sucesso!"})
+
+
+@app.route("/comissao", methods=["GET"])
+def read_comissao():
+    comissoes = Comissao.query.all()
+    comissoes = [
+        {"id_comissao": comissao.id_comissao, "nome_comissao": comissao.nome_comissao}
+        for comissao in comissoes
+    ]
+    return jsonify(comissoes)
+
+
+@app.route("/comissao/<int:id_comissao>", methods=["GET"])
+def read_comissao_id(id_comissao):
+    comissao = Comissao.query.get(id_comissao)
     return jsonify(
-        [{"id": c.id_classe, "descricao": c.descricao_classe} for c in classes]
+        {"id_comissao": comissao.id_comissao, "nome_comissao": comissao.nome_comissao}
     )
 
 
-# ✅ Adicionar uma nova classe de produto
-@app.route("/classes", methods=["POST"])
-def add_classe():
+@app.route("/comissao/<int:id_comissao>", methods=["PUT"])
+def update_comissao(id_comissao):
     data = request.json
-    classe = Classe(descricao_classe=data["descricao_classe"])
-    db.session.add(classe)
+    comissao = Comissao.query.get(id_comissao)
+    comissao.nome_comissao = data["nome_comissao"]
+    comissao.cpf_comissao = data["cpf_comissao"]
+    comissao.cargo_comissao = data["cargo_comissao"]
+    comissao.id_modalidade = data["id_modalidade"]
     db.session.commit()
-    return jsonify({"message": "Classe adicionada com sucesso!"})
+    return jsonify({"message": "Comissão atualizada com sucesso!"})
 
 
-# ✅ Listar todos os produtos
-@app.route("/produtos", methods=["GET"])
-def get_produtos():
-    produtos = Produto.query.all()
+@app.route("/comissao/<int:id_comissao>", methods=["DELETE"])
+def delete_comissao(id_comissao):
+    comissao = Comissao.query.get(id_comissao)
+    db.session.delete(comissao)
+    db.session.commit()
+    return jsonify({"message": "Comissão deletada com sucesso!"})
+
+
+# crud para modalidade
+@app.route("/modalidade", methods=["POST"])
+def create_modalidade():
+    data = request.json
+    modalidade = Modalidade(
+        nome_modalidade=data["nome_modalidade"],
+        saldo_modalidade=data["saldo_modalidade"],
+    )
+    db.session.add(modalidade)
+    db.session.commit()
+    return jsonify({"message": "Modalidade criada com sucesso!"})
+
+
+@app.route("/modalidade", methods=["GET"])
+def read_modalidade():
+    modalidades = Modalidade.query.all()
+    modalidades = [
+        {
+            "id_modalidade": modalidade.id_modalidade,
+            "nome_modalidade": modalidade.nome_modalidade,
+        }
+        for modalidade in modalidades
+    ]
+    return jsonify(modalidades)
+
+
+@app.route("/modalidade/<int:id_modalidade>", methods=["GET"])
+def read_modalidade_id(id_modalidade):
+    modalidade = Modalidade.query.get(id_modalidade)
     return jsonify(
-        [{"id": p.id_produto, "descricao": p.descricao_produto} for p in produtos]
+        {
+            "id_modalidade": modalidade.id_modalidade,
+            "nome_modalidade": modalidade.nome_modalidade,
+        }
     )
 
 
-# ✅ Obter detalhes de um produto específico
-@app.route("/produtos/<int:id>", methods=["GET"])
-def get_produto(id):
-    produto = Produto.query.get(id)
-    if produto:
-        return jsonify(
-            {
-                "id": produto.id_produto,
-                "descricao": produto.descricao_produto,
-                "id_classe": produto.id_classe,
-                "id_vendedor": produto.id_vendedor,
-            }
-        )
-    return jsonify({"message": "Produto não encontrado"}), 404
-
-
-# ✅ Adicionar um novo produto
-@app.route("/produtos", methods=["POST"])
-def add_produto():
+@app.route("/modalidade/<int:id_modalidade>", methods=["PUT"])
+def update_modalidade(id_modalidade):
     data = request.json
-
-    vendedor = Vendedor.query.filter_by(cpf_vendedor=data["cpf_vendedor"]).first()
-    if not vendedor:
-        vendedor = Vendedor(
-            nome_vendedor=data["nome_vendedor"],
-            cpf_vendedor=data["cpf_vendedor"],
-            cidade_vendedor=data["cidade_vendedor"],
-        )
-        db.session.add(vendedor)
-        db.session.commit()
-
-    produto = Produto(
-        descricao_produto=data["descricao_produto"],
-        id_classe=data["id_classe"],
-        id_vendedor=vendedor.id_vendedor,
-    )
-    db.session.add(produto)
+    modalidade = Modalidade.query.get(id_modalidade)
+    modalidade.nome_modalidade = data["nome_modalidade"]
+    modalidade.saldo_modalidade = data["saldo_modalidade"]
     db.session.commit()
-    return jsonify({"message": "Produto adicionado com sucesso!"})
+    return jsonify({"message": "Modalidade atualizada com sucesso!"})
 
 
-# ✅ Remover um produto pelo ID
-@app.route("/produtos/<int:id>", methods=["DELETE"])
-def delete_produto(id):
-    produto = Produto.query.get(id)
-    if produto:
-        db.session.delete(produto)
-        db.session.commit()
-        return jsonify({"message": "Produto removido com sucesso!"})
-    return jsonify({"message": "Produto não encontrado"}), 404
+@app.route("/modalidade/<int:id_modalidade>", methods=["DELETE"])
+def delete_modalidade(id_modalidade):
+    modalidade = Modalidade.query.get(id_modalidade)
+    db.session.delete(modalidade)
+    db.session.commit()
+    return jsonify({"message": "Modalidade deletada com sucesso!"})
 
 
-# ✅ Atualizar um produto pelo ID
-@app.route("/produtos/<int:id>", methods=["PUT"])
-def update_produto(id):
+# crud para modalidade_atleta
+@app.route("/modalidade_atleta", methods=["POST"])
+def create_modalidade_atleta():
     data = request.json
-    produto = Produto.query.get(id)
+    modalidade_atleta = Modalidade_Atleta(
+        id_modalidade=data["id_modalidade"],
+        id_atleta=data["id_atleta"],
+    )
+    db.session.add(modalidade_atleta)
+    db.session.commit()
+    return jsonify({"message": "Modalidade do atleta criada com sucesso!"})
 
-    if produto:
-        produto.descricao_produto = data.get(
-            "descricao_produto", produto.descricao_produto
-        )
-        produto.id_classe = data.get("id_classe", produto.id_classe)
-        db.session.commit()
-        return jsonify({"message": "Produto atualizado com sucesso!"})
 
-    return jsonify({"message": "Produto não encontrado"}), 404
+@app.route("/modalidade_atleta", methods=["GET"])
+def read_modalidade_atleta():
+    modalidades_atletas = Modalidade_Atleta.query.all()
+    modalidades_atletas = [
+        {
+            "id_modalidade_atleta": modalidade_atleta.id_modalidade_atleta,
+            "id_modalidade": modalidade_atleta.id_modalidade,
+        }
+        for modalidade_atleta in modalidades_atletas
+    ]
+    return jsonify(modalidades_atletas)
+
+
+@app.route("/modalidade_atleta/<int:id_modalidade_atleta>", methods=["GET"])
+def read_modalidade_atleta_id(id_modalidade_atleta):
+    modalidade_atleta = Modalidade_Atleta.query.get(id_modalidade_atleta)
+    return jsonify(
+        {
+            "id_modalidade_atleta": modalidade_atleta.id_modalidade_atleta,
+            "id_modalidade": modalidade_atleta.id_modalidade,
+        }
+    )
+
+
+@app.route("/modalidade_atleta/<int:id_modalidade_atleta>", methods=["PUT"])
+def update_modalidade_atleta(id_modalidade_atleta):
+    data = request.json
+    modalidade_atleta = Modalidade_Atleta.query.get(id_modalidade_atleta)
+    modalidade_atleta.id_modalidade = data["id_modalidade"]
+    modalidade_atleta.id_atleta = data["id_atleta"]
+    db.session.commit()
+    return jsonify({"message": "Modalidade do atleta atualizada com sucesso!"})
+
+
+@app.route("/modalidade_atleta/<int:id_modalidade_atleta>", methods=["DELETE"])
+def delete_modalidade_atleta(id_modalidade_atleta):
+    modalidade_atleta = Modalidade_Atleta.query.get(id_modalidade_atleta)
+    db.session.delete(modalidade_atleta)
+    db.session.commit()
+    return jsonify({"message": "Modalidade do atleta deletada com sucesso!"})
+
+
+# crud para campeonato
+@app.route("/campeonato", methods=["POST"])
+def create_campeonato():
+    data = request.json
+    campeonato = Campeonato(
+        nome_campeonato=data["nome_campeonato"],
+        data_inicio=data["data_inicio"],
+        data_fim=data["data_fim"],
+        custo_por_pessoa=data["custo_por_pessoa"],
+    )
+    db.session.add(campeonato)
+    db.session.commit()
+    return jsonify({"message": "Campeonato criado com sucesso!"})
+
+
+@app.route("/campeonato", methods=["GET"])
+def read_campeonato():
+    campeonatos = Campeonato.query.all()
+    campeonatos = [
+        {
+            "id_campeonato": campeonato.id_campeonato,
+            "nome_campeonato": campeonato.nome_campeonato,
+        }
+        for campeonato in campeonatos
+    ]
+    return jsonify(campeonatos)
+
+
+@app.route("/campeonato/<int:id_campeonato>", methods=["GET"])
+def read_campeonato_id(id_campeonato):
+    campeonato = Campeonato.query.get(id_campeonato)
+    return jsonify(
+        {
+            "id_campeonato": campeonato.id_campeonato,
+            "nome_campeonato": campeonato.nome_campeonato,
+        }
+    )
+
+
+@app.route("/campeonato/<int:id_campeonato>", methods=["PUT"])
+def update_campeonato(id_campeonato):
+    data = request.json
+    campeonato = Campeonato.query.get(id_campeonato)
+    campeonato.nome_campeonato = data["nome_campeonato"]
+    campeonato.data_inicio = data["data_inicio"]
+    campeonato.data_fim = data["data_fim"]
+    campeonato.custo_por_pessoa = data["custo_por_pessoa"]
+    db.session.commit()
+    return jsonify({"message": "Campeonato atualizado com sucesso!"})
+
+
+@app.route("/campeonato/<int:id_campeonato>", methods=["DELETE"])
+def delete_campeonato(id_campeonato):
+    campeonato = Campeonato.query.get(id_campeonato)
+    db.session.delete(campeonato)
+    db.session.commit()
+    return jsonify({"message": "Campeonato deletado com sucesso!"})
+
+
+# crud para modalidade_campeonato
+@app.route("/modalidade_campeonato", methods=["POST"])
+def create_modalidade_campeonato():
+    data = request.json
+    modalidade_campeonato = Modalidade_Campeonato(
+        id_campeonato=data["id_campeonato"],
+        id_modalidade=data["id_modalidade"],
+    )
+    db.session.add(modalidade_campeonato)
+    db.session.commit()
+    return jsonify({"message": "Modalidade do campeonato criada com sucesso!"})
+
+
+@app.route("/modalidade_campeonato", methods=["GET"])
+def read_modalidade_campeonato():
+    modalidades_campeonatos = Modalidade_Campeonato.query.all()
+    modalidades_campeonatos = [
+        {
+            "id_modalidade_campeonato": modalidade_campeonato.id_modalidade_campeonato,
+            "id_campeonato": modalidade_campeonato.id_campeonato,
+        }
+        for modalidade_campeonato in modalidades_campeonatos
+    ]
+    return jsonify(modalidades_campeonatos)
+
+
+@app.route("/modalidade_campeonato/<int:id_modalidade_campeonato>", methods=["GET"])
+def read_modalidade_campeonato_id(id_modalidade_campeonato):
+    modalidade_campeonato = Modalidade_Campeonato.query.get(id_modalidade_campeonato)
+    return jsonify(
+        {
+            "id_modalidade_campeonato": modalidade_campeonato.id_modalidade_campeonato,
+            "id_campeonato": modalidade_campeonato.id_campeonato,
+        }
+    )
+
+
+@app.route("/modalidade_campeonato/<int:id_modalidade_campeonato>", methods=["PUT"])
+def update_modalidade_campeonato(id_modalidade_campeonato):
+    data = request.json
+    modalidade_campeonato = Modalidade_Campeonato.query.get(id_modalidade_campeonato)
+    modalidade_campeonato.id_campeonato = data["id_campeonato"]
+    modalidade_campeonato.id_modalidade = data["id_modalidade"]
+    db.session.commit()
+    return jsonify({"message": "Modalidade do campeonato atualizada com sucesso!"})
+
+
+@app.route("/modalidade_campeonato/<int:id_modalidade_campeonato>", methods=["DELETE"])
+def delete_modalidade_campeonato(id_modalidade_campeonato):
+    modalidade_campeonato = Modalidade_Campeonato.query.get(id_modalidade_campeonato)
+    db.session.delete(modalidade_campeonato)
+    db.session.commit()
+    return jsonify({"message": "Modalidade do campeonato deletada com sucesso!"})
 
 
 # Iniciar a aplicação
